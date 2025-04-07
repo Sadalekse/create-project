1d03aec43162cc9d75ed6e9e8d76218e2ce9ee23
Sadalekse <Sadalekse@gmail.com>
2025-04-07 17:27:56 +0500

    Define API routes for auth, users, access, books and external search

---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\LibraryAccessController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Пользователи
+    Route::get('/users', [UserController::class, 'index']);
+    Route::post('/library/access', [LibraryAccessController::class, 'grantAccess']);
+
+    // Книги
+    Route::get('/books', [BookController::class, 'index']);
+    Route::post('/books', [BookController::class, 'store']);
+    Route::get('/books/{id}', [BookController::class, 'show']);
+    Route::put('/books/{id}', [BookController::class, 'update']);
+    Route::delete('/books/{id}', [BookController::class, 'destroy']);
+
+    // Книги других пользователей (если есть доступ)
+    Route::get('/users/{id}/books', [BookController::class, 'getUserBooks']);
+
+    // Поиск и сохранение внешних книг
+    Route::get('/books/search', [BookController::class, 'searchBooks']);
+    Route::post('/books/save', [BookController::class, 'saveExternalBook']);
+});
